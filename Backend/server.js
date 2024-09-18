681e242bd7028c384b24db4c42f32c4cabd29878
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-09-18 18:17:59 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,13 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
