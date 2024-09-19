44e366693c5ee51a728fc503ec875c5ab4e647c3
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-09-19 18:26:25 +0530

    Basic setup for message sending

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
